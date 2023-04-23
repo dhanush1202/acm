66f0fc5a8a6156c453a3f26643df2b0cc7e123fe
Samhithreddykosana <95043320+Samhithreddykosana@users.noreply.github.com>
2023-04-23 20:33:17 +0530

    preloader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React, {useState} from "react";
 import Homepage from "./components/Homepage";
+import PreLoader  from "./components/PreLoader" 
 
 function App() {
+
+   
+
   return (
-    <>
+     <>
+     <PreLoader/>
     <Homepage />
 
     </>
---
+++ src/components/PreLoader.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react'
+import "./Preloader.css"
+import { preLoaderAnim } from '../animations'
+import logo from '../images/logo1.png'
+
+
+const PreLoader = ()=> {
+    useEffect(()=>{
+        preLoaderAnim()
+    },[])
+  return (
+    <div className="preloader">
+        <div className="texts-container">
+            <span>A</span>
+            <span>C</span>
+            <span>M</span>
+            
+            
+        </div>
+    </div>
+  )
+}
+
+export default PreLoader;
