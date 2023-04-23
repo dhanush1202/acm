a5caff40632de4a301228fb0850407f016c8ec77
anirudh-bukka <anirudh.bukka@gmail.com>
2023-04-23 20:48:32 +0530

    Added Events Page folder, Nithin will update his additions.

---
+++ src/components/Events/timelineContent.js
@@ -0,0 +1,93 @@
+let timelineContent = [
+    {
+      id: 1,
+      title: "Event 6",
+      location: "VNR VJIET",
+      description:
+        "Dance.",
+      buttonText: "View Gallery",
+      date: "2023",
+      icon: "work",
+    },
+    {
+      id: 2,
+      title: "Event 5",
+      location: "VNR VJIET",
+      description:
+        "Pass time.",
+      buttonText: "View Gallery",
+      date: "2023",
+      icon: "work",
+    },
+    {
+      id: 3,
+      title: "Event 4",
+      location: "VNR VJIET",
+      description:
+        "Jackshit",
+      buttonText: "View Gallery",
+      date: "2022-2023",
+      icon: "work",
+    },
+    {
+      id: 4,
+      title: "Event 3",
+      location: "VNR VJIET",
+      description:
+        "Murali Mohan Sir Gangsta",
+      buttonText: "View Gallery",
+      date: "2022-2022",
+      icon: "work",
+    },
+    {
+      id: 5,
+      title: "Event 2",
+      location: "VNR VJIET",
+      description:
+        "I am out of deadass descriptions.",
+      buttonText: "College Projects",
+      date: "2021-2022",
+      icon: "work",
+    },
+    {
+      id: 6,
+      title: "Event 1",
+      location: "VNR VJIET",
+      description:
+        "College - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+      date: "2021",
+      icon: "work",
+    },
+    {
+      id: 7,
+      title: "Event 6",
+      location: "VNR VJIET",
+      description:
+        "Dance.",
+      buttonText: "View Gallery",
+      date: "2023",
+      icon: "work",
+    },
+    {
+      id: 8,
+      title: "Event 5",
+      location: "VNR VJIET",
+      description:
+        "Pass time.",
+      buttonText: "View Gallery",
+      date: "2023",
+      icon: "work",
+    },
+    {
+      id: 9,
+      title: "Event 4",
+      location: "VNR VJIET",
+      description:
+        "Jackshit",
+      buttonText: "View Gallery",
+      date: "2022-2023",
+      icon: "work",
+    }
+  ];
+  
+  export default timelineContent;
