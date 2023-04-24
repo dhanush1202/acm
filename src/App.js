e0883f4373d5344b56118e28d15cd92099cfb8b3
charan-24 <charanpasupunooti137@gmail.com>
2023-04-24 08:07:45 +0530

    added contact and membership-page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Homepage from "./components/Homepage";
-
+import { MembershipInfo } from "./components/MembershipInfo";
+import { ContactForm } from "./components/Contact";
 function App() {
   return (
     <>
     <Homepage />
-
+    <MembershipInfo />
+    <ContactForm />
     </>
   );
 }
---
+++ src/components/MembershipInfo.js
@@ -0,0 +1,91 @@
+import React, { Component }  from 'react';
+import Card from 'react-bootstrap/Card';
+import '../styles/MembershipInfo.css';
+import img1 from '../images/anonynous.png';
+// import { CardGroup } from 'react-bootstrap';
+export const MembershipInfo = () => {
+    const arr=[1,2,3];
+    arr.length=3;
+return(
+    <>
+        <div className='membership'>
+            <div className='title'>
+                <h1>Membership</h1>
+            </div>
+            <p className="membershipdesc">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            
+            </p>
+            <div className='membershiptypes'>
+                <div className="membertype">
+                    <h5>Membership Type-1</h5>
+                    <p className="typedesc">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                
+                    </p>
+                    <button><b>Contact Us</b></button>
+                </div>
+                <div className="membertype">
+                    <h5>Membership Type-2</h5>
+                    <p className="typedesc">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                
+                    </p>
+                        <button><b>Register</b></button>
+                </div>
+            </div>
+        </div>
+        <div  className="testimony-h1" style={{height:"90px"}}>
+            <h1>Testimonials</h1>
+        </div>
+        <div className='testimonials'>
+        {
+            arr.map(()=>{
+                return (
+                    <>
+                       
+                        <Card className='testimony'>
+                                <Card.Body>
+                                    <img src={img1} alt='A Person'/>
+                                    <h3>Something</h3>
+                                    <p>
+                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                    </p>
+                                </Card.Body>
+                            </Card> 
+                       
+                    </>
+                )
+            })
+        }
+        
+        {/* <Card className='testimony'>
+            <Card.Body>
+            
+                <img src={img1} alt='A Person'/>
+                <h3>Something</h3>
+                <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+            </Card.Body>
+        </Card>
+        <Card className='testimony'>
+            <Card.Body>
+                <img src={img1} alt='A Person'/>
+                <h3>Something</h3>
+                <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+            </Card.Body>
+        </Card>
+        <Card className='testimony'>
+            <Card.Body>
+                <img src={img1} alt='A Person'/>
+                <h3>Something</h3>
+                <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+            </Card.Body>
+        </Card> */}
+        </div>
+    </>
+
+);
+}
